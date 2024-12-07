7b73db6abb3cd6a34e3d6635536bfb04d426210b
Terentev Danill <pyzan55@gmail.com>
2024-12-07 22:47:27 +0000

    changes

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -4,4 +4,4 @@ from .models import Post, Group, Comment, Follow
 
 @admin.register(Post, Group, Comment, Follow)
 class BlogAdmin(admin.ModelAdmin):
-    pass
+    ...
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,28 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
-
-    def __str__(self):
-        return self.text
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
-
-
 class Follow(models.Model):
     user = models.ForeignKey(
         User, verbose_name='Подписчик',
@@ -64,3 +42,33 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(
+        upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        verbose_name='Сообщество',
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
